df08c85b02bad164ac6d5c3da52a39ee80366223
mdemick <jacobhannah494>
2016-03-26 00:56:17 -0400

    updates

--- apps/drupal/htdocs/sites/all/themes/stability/templates/views-view-fields--tournaments--block-2.tpl.php
+++ apps/drupal/htdocs/sites/all/themes/stability/templates/views-view-fields--tournaments--block-2.tpl.php
@@ -1,2 +1,23 @@
+<?php
+/*
+<!--
 
-<div style="font-size: 20px;font-family: 'Oswald', sans-serif;margin: 0 0 0 0;"><?php print $fields['name']->content; ?></div>
+[field_game_image_sm_1] == Content: game_image_sm
+[field_tournament_challonge_url] == Content: Tournament_Challonge_URL
+[field_tournament_name] == Content: Name
+
+<ul class="latest-posts-list">
+    <?php print $rows; ?>
+</ul>
+-->
+*/
+?>
+
+<li>
+    <strong>
+        <a href="  <?php print '/node/493/' . $fields['field_tournament_challonge_url']->content; ?> ">
+            <?php print $fields['field_game_image_sm_1']->content; ?>
+            <?php print $fields['field_tournament_name']->content; ?>
+        </a>
+    </strong>
+</li>
